8fe04a260712d3aa888a23818cfece0c41872edf
Nicolas Kler <nicolasjosekler@gmail.com>
2023-04-13 19:04:21 -0300

    BaSP-W05-2023: Add all excercises related to for

--- Week-05/exercises/04-if-else.js
+++ Week-05/exercises/04-if-else.js
@@ -13,7 +13,7 @@ if (randnum<0.5)[
 else[
     console.log('Greater than or equal to 0,5')
 ];
-/*Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+/*b. Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
 “Bebe” si la edad es menor a 2 años;
 “Niño” si la edad es entre 2 y 12 años;
 “Adolescente” entre 13 y 19 años;
